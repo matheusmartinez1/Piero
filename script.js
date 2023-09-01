a004b88b8ddd449c91a7b7d1b4210ae6ef355c95
matheusmartinez1 <143743137+matheusmartinez1@users.noreply.github.com>
2023-08-31 22:06:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const calculateButton = document.getElementById("calculate-button");
-    const cepInput = document.getElementById("cep");
     const freteInfo = document.getElementById("frete-info");
 
     calculateButton.addEventListener("click", function() {
-        const cepValue = cepInput.value;
-        const freteValue = calculateFrete(cepValue);
-        
-        if (freteValue !== null) {
-            freteInfo.textContent = `Frete calculado: R$${freteValue.toFixed(2)}`;
-        } else {
-            freteInfo.textContent = "CEP inválido. Verifique e tente novamente.";
-        }
+        const cep = document.getElementById("cep").value;
+        // Lógica para calcular o frete com base no CEP
+        // e exibir o resultado em freteInfo
+        freteInfo.textContent = `O frete para o CEP ${cep} é R$10,00.`;
     });
-
-    function calculateFrete(cep) {
-        // Implemente a lógica de cálculo real do frete aqui
-        // Retorna o valor do frete ou null se o CEP for inválido
-    }
 });
