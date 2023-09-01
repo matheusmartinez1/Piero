c216839c721524509b98c3f33cb19b653a6ba26f
matheusmartinez1 <143743137+matheusmartinez1@users.noreply.github.com>
2023-08-31 22:13:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,65 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const calculateButton = document.getElementById("calculate-button");
-    const freteInfo = document.getElementById("frete-info");
+    const buyButtons = document.querySelectorAll(".buy-button");
+    const cartButtons = document.querySelectorAll(".cart-button");
+    const cartItems = document.getElementById("cart-items");
+    const cartTotal = document.getElementById("cart-total");
+    const checkoutButton = document.getElementById("checkout-button");
 
-    calculateButton.addEventListener("click", function() {
-        const cep = document.getElementById("cep").value;
-        // Lógica para calcular o frete com base no CEP
-        // e exibir o resultado em freteInfo
-        freteInfo.textContent = `O frete para o CEP ${cep} é R$10,00.`;
+    let cart = [];
+    let total = 0;
+
+    function updateCart() {
+        cartItems.innerHTML = "";
+        total = 0;
+
+        cart.forEach(item => {
+            const itemDiv = document.createElement("div");
+            itemDiv.innerHTML = `
+                <p>Produto: ${item.name}</p>
+                <p>Preço: R$${item.price.toFixed(2)}</p>
+            `;
+            cartItems.appendChild(itemDiv);
+            total += item.price;
+        });
+
+        cartTotal.textContent = `R$${total.toFixed(2)}`;
+    }
+
+    buyButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            const product = button.getAttribute("data-product");
+            const productDetails = getProductDetails(product);
+            cart.push({ name: productDetails.name, price: productDetails.price });
+            updateCart();
+        });
+    });
+
+    cartButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            const product = button.getAttribute("data-product");
+            const productDetails = getProductDetails(product);
+            cart.push({ name: productDetails.name, price: productDetails.price });
+            updateCart();
+        });
+    });
+
+    function getProductDetails(product) {
+        // Substitua por uma função que retorna os detalhes reais do produto
+        const products = {
+            copo1: { name: "Copo Tradicional", price: 15.00 },
+            // Substitua os outros produtos aqui
+        };
+        return products[product];
+    }
+
+    checkoutButton.addEventListener("click", function() {
+        if (cart.length > 0) {
+            // Simule uma finalização de compra
+            alert("Compra finalizada! Obrigado por comprar na PIERO Store!");
+            cart = [];
+            updateCart();
+        } else {
+            alert("Seu carrinho está vazio. Adicione produtos antes de finalizar a compra.");
+        }
     });
 });
